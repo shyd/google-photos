49f5125054e6fd3faf88c0e2ec5950ec65627695
Dennis Schütt <dennis@shyd.de>
2019-10-29 20:05:26 +0100

    added gitignore, docker, env config

--- REST/PhotoFrame/config.js
+++ REST/PhotoFrame/config.js
@@ -17,17 +17,26 @@
 const config = {};
 
 // The OAuth client ID from the Google Developers console.
-config.oAuthClientID = 'ADD YOUR CLIENT ID';
+//config.oAuthClientID = 'ADD YOUR CLIENT ID';
+config.oAuthClientID = process.env.CLIENT_ID;
 
 // The OAuth client secret from the Google Developers console.
-config.oAuthclientSecret = 'ADD YOUR CLIENT SECRET';
+//config.oAuthclientSecret = 'ADD YOUR CLIENT SECRET';
+config.oAuthclientSecret = process.env.CLIENT_SECRET;
 
 // The callback to use for OAuth requests. This is the URL where the app is
 // running. For testing and running it locally, use 127.0.0.1.
+//config.oAuthCallbackUrl = 'http://127.0.0.1:8080/auth/google/callback';
 config.oAuthCallbackUrl = 'http://127.0.0.1:8080/auth/google/callback';
+if (process.env.CALLBACK_URL !== undefined) {
+  config.oAuthCallbackUrl = process.env.CALLBACK_URL;
+}
 
 // The port where the app should listen for requests.
 config.port = 8080;
+if (process.env.PORT !== undefined) {
+  config.port = process.env.PORT;
+}
 
 // The scopes to request. The app requires the photoslibrary.readonly and
 // plus.me scopes.
